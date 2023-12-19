49b5b973747a0f968bd8e89e18c1b943b82e74b2
Dzaki Rozaan <dzakirz.dev@gmail.com>
2023-12-19 19:14:53 +0700

    create development info page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Dzaki</h1>
-    </div>
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-xl md:text-2xl text-zinc-700 font-semibold">
+        Welcome to dzakirozaan.com !
+      </h1>
+      <Image
+        className="mt-2"
+        src="/development.svg"
+        alt="Under Construction"
+        width={250}
+        height={100}
+        priority
+      />
+      <h1 className="text-lg md:text:xl text-zinc-700 font-bold mt-2">Sorry</h1>
+      <p className="text-sm md:tex-base text-zinc-500 text-center">
+        This website is still under development, please come back later. Thank
+        you !
+      </p>
+      <a
+        className="text-blue-600 underline"
+        href="https://wa.me/6283154641298"
+        target="_blank"
+      >
+        contact me
+      </a>
+    </main>
   )
 }
