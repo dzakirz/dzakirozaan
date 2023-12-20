f75ed3b5a318e456fe0ea8acddedcc6f1cad499d
Dzaki Rozaan <dzakirz.dev@gmail.com>
2023-12-20 07:05:19 +0700

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         height={100}
         priority
       />
-      <h1 className="text-lg md:text:xl text-zinc-700 font-bold mt-2">Sorry</h1>
+      <h1 className="text-lg md:text:xl text-zinc-700 font-bold mt-2">Ups, Sorry...</h1>
       <p className="text-sm md:tex-base text-zinc-500 text-center">
         This website is still under development, please come back later. Thank
         you !
@@ -24,7 +24,7 @@ export default function Home() {
         href="https://wa.me/6283154641298"
         target="_blank"
       >
-        contact me
+        button
       </a>
     </main>
   )
